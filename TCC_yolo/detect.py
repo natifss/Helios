f18ca409f432519b8decf832c14612a49ddafd89
Natali <natali-.santos@hotmail.com>
2025-03-29 13:46:53 -0300

    deepface update

--- TCC_yolo/detect.py
+++ TCC_yolo/detect.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 import cv2
-
+import numpy as np
+from deepface import DeepFace
 
 model = YOLO("yolov8n.pt") 
 
-video_path = 0
+video_path = 0 
 cap = cv2.VideoCapture(video_path)
-
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
@@ -19,22 +19,24 @@ while cap.isOpened():
             x1, y1, x2, y2 = map(int, box.xyxy[0]) 
             conf = box.conf[0].item() 
             cls = int(box.cls[0]) 
-
             if cls == 0:
-                label = f"Pessoa ({conf:.2f})"
-                color = (0, 255, 0) # Verde
-            else:
-                label = f"Outro ({conf:.2f})"
-                color = (255, 0, 0) # Azul
-            
-            cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
-            cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-    color, 2)
-            
-        cv2.imshow("YOLOv8 Detecção", frame)
+                face = frame[y1:y2, x1:x2]
+                if face.shape[0] > 0 and face.shape[1] > 0:
+                    try:
+                        analysis = DeepFace.analyze(face, actions=["gender"], enforce_detection=False)
+                        gender = analysis[0]["dominant_gender"]
+                        if gender == "Woman":
+                            label = f"Mulher ({conf:.2f})"
+                            color = (0, 0, 255) 
+                            cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+                            cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                    except Exception as e:
+                        print("Erro na análise de gênero:", e)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        
+    cv2.imshow("YOLOv8 - Detecção de Mulheres", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
     cap.release()
     cv2.destroyAllWindows()
